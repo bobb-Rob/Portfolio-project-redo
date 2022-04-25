5d76d17ed9bb12e7306ebc50a06e76f2780cc6e4
Robertson Akpan <robertsonakpan@gmail.com>
2022-04-25 18:48:16 +0100

    Initial commit

---
+++ src/NavBar.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+class Navbar extends React.Component {
+
+  constructor(props) {
+    super(props)  
+  }
+
+  render() {  
+    return (
+      <div>Nav</div>
+    )
+  }
+}
+
+export default Navbar
